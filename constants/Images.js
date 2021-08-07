53ebf246ef8a15e9e2275fa8522afe42aaf03a5f
Mr-Rong <42691849+Mr-Rong@users.noreply.github.com>
2021-08-07 09:08:38 +0800

    Add files via upload

---
+++ constants/Images.js
@@ -0,0 +1,17 @@
+const wallieLogo = require(../assets/images/wallie-LogBox.png);
+const banner = require(../assets/images/wallie-LogBox.png);
+const promoBanner = require(../assets/images/wallie-LogBox.png);
+const focus = require(../assets/images/wallie-LogBox.png);
+
+//Dummy
+const usFlag = require(../assets/images/wallie-LogBox.png);
+
+export default {
+    walleLogo,
+    banner,
+    promoBanner,
+    Focus,
+
+    //Dummy
+    usFlag
+}
