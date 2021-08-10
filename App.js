9fdca8911d3ff7e0bccfd5c9849283029c4c4a24
Mr-Rong <ronglu10@gmail.com>
2021-08-11 06:45:17 +0800

    test

--- App.js
+++ App.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createStackNavigation, createStackNavigator } from "@react-navigation/stack"
+import { NavigationContainer, DefaultTheme  } from '@react-navigation/native';
 
 import { SignUp } from './Screens';
 
-
 const theme = {
   ...DefaultTheme,
-  colours:{
+  colors:{
     ...DefaultTheme.colors,
-    border: "transparent"
+    border:"transparent"
   }
 }
 
 const Stack = createStackNavigator()
 
-const App = () => {
+
+const App =() =>{
   return (
-      <NavigationContainer theme={theme}>
-        <Stack.Navigator
-          ScreenOptions={{
-            headerShown: false
-          }}
-          initialRouteName={'SignUp'}
-        >
-
-
-          <Stack.Screen name="SignUp" component={SignUp}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={'SignUp'}
+      >
+        <Stack.Screen name="SignUp" component={SignUp}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  
   )
+
 }
 
-
+export default App;
